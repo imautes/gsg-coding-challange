8426c53e059cd79b9aafdaa8c3a7fc5a1fa4e636
Istvan Mosonyi <istvan.mosonyi@imautes.onmicrosoft.com>
2022-10-23 14:22:21 +0200

    Created empty SB project

---
+++ src/main/java/com/gsg/codingchallenge/GsgCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gsg.codingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsgCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsgCodingChallengeApplication.class, args);
+    }
+
+}
